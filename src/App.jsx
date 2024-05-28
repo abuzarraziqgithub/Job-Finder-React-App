0c9b0fb7dc6fb7b74b1d5062dc225c54deebe932
abuzarraziqgithub <abuzarraziqo@gmail.com>
2024-05-28 20:48:16 +0500

    JSX-Crash-Course

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,32 @@ import React from "react";
 //* WE CAN ONLY RETURN JUST ONE ELEMENT AS A PARENT ELEMENT
 //* WE CAN USE FRAGMENT (EMPTY HTML ELEMENT) IF A DEV DON'T WANNA USE DIV
 //*ABOVE THE RETURN, WE WRITE JAVASCRIPT
+//* WE CAN WRITE INLINE STYLING INSIDE JSX
 const App = () => {
+  // WE CAN PASS IN A VARIABLE AND IT WILL RENDER IT'S VALUE
+  const name = "Abuzar";
+  // WE CAN ALSO DO COMPUTATION INSIDE JSX
+  const x = 10;
+  const y = 20;
+  // WE CAN ALSO LOOP OVER THE ARRAY USING MAP METHOD FOR ENDERING EACH ELEMENT
+
+  // WE CAN ALSO APPLY CONDITION USING TERNARY OPERATORS,LOGICAL OPERATORS BUT NOT THE STATEMENTS.
+  const loggedIn = true;
+
+  const names = ["Shafqat", "Essa", "Mujtaba", "Gulalai"];
   return (
     <>
-      <div className="text-9xl">Hello world</div>
+      <div className="text-5xl">Hello {name}</div>
+      <p style={{ color: "red", fontSize: "20px" }}>
+        The sum of {x} and {y} is {x + y}
+      </p>
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+      {/* {loggedIn ? <h1> Hello Member</h1> : <h1>Hello Guest</h1>} */}
+      {loggedIn && <h1>Hello Member</h1>}
     </>
   );
 };
